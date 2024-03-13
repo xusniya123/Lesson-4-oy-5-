0402e0cd1c9541073e7c8fe66a5f4986bfdde709
xusniya123 <xusniya12345678@gmail.com>
2024-03-13 14:10:04 +0500

    Lesson 4 moth (5)

---
+++ main.js
@@ -0,0 +1,24 @@
+// let text = "Yo'ldoshmurodova";
+// let uzunligi = text.length;
+// alert(uzunligi);
+
+// let text = "Bunyodbek, Zubayr, Ali";
+// let part = text.slice(7,13)
+
+// let text1 = "Bunyodbek, Zubayr, Ali";
+// let part1 = text.slice(7)
+
+// let text2 = "Bunyodbek, Zubayr, Ali";
+// let part2 = text.slice(-3)
+
+
+let name = (prompt("Ismingizni kiriting!!"))
+let test =name.includes("X")
+if (test){
+alert("Bu harf mavjud")
+}else{
+    alert("Bu harf mavjud emas")
+}
+alert(test);
+
+
